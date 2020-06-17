6122fb81d5683e68420f75511e5f95ae22c9ed48
anirban-s <baban.chrome@gmail.com>
2020-06-17 19:02:51 +0530

    enhancing the app

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import { Post } from './post.model';
 
 
 @Injectable({
@@ -12,7 +14,7 @@ export class PostsService {
     private posts: Post[] = [];
     private postUpdated = new Subject<Post[]>();
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     getPosts() {
         this.http
@@ -38,6 +40,10 @@ export class PostsService {
         return this.postUpdated.asObservable();
     }
 
+    getPost(id: string) {
+        return this.http.get<{_id:string, title:string, content:string}>("http://localhost:3000/api/posts/"+ id);
+    }
+
     addPost(title:string, content:string){
         const post: Post = {id:null, title:title, content:content};
         this.http.post<{message:string, postId:string}>('http://localhost:3000/api/posts', post)
@@ -46,8 +52,22 @@ export class PostsService {
                 post.id = id;
                 this.posts.push(post);
                 this.postUpdated.next([...this.posts]);
+                this.router.navigate(["/"]);
             });
         
+    }
+
+    updatePost(id:string, title:string, content:string){
+        const post: Post = {id: id, title: title, content: content };
+        this.http.put('http://localhost:3000/api/posts/'+ id, post)
+            .subscribe(response => {
+                const updatedPosts = [...this.posts];
+                const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+                updatedPosts[oldPostIndex] = post;
+                this.posts = updatedPosts;
+                this.postUpdated.next([...this.posts]);
+                this.router.navigate(["/"]);
+            })
     }
 
 
