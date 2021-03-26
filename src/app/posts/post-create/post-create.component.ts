e1058421bd2d09c954fb02e4e0df5cdf4407d41a
anirban-s <baban.chrome@gmail.com>
2021-03-27 03:24:42 +0530

    completed UI

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
-  enteredValue = '';
-  newPost = 'NO Content';
+export class PostCreateComponent {
 
-  constructor() { }
+  constructor(public postsService: PostsService) { }
 
-  ngOnInit(): void {
-  }
-
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  onAddPost(form: NgForm) {
+    if (form.invalid){
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
 }
