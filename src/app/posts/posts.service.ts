1ee77229c2c899c159736047c0a7f0d8978685ac
anirban-s <baban.chrome@gmail.com>
2021-03-28 04:49:29 +0530

    update service is completed

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Post } from "./post.model";
@@ -9,11 +10,9 @@ import { Post } from "./post.model";
 })
 export class PostsService {
     private posts: Post[] = [];
-    private postsUpdated = new Subject<Post[]>()
+    private postsUpdated = new Subject<Post[]>();
 
-    constructor(private http: HttpClient) {
-
-    }
+    constructor(private http: HttpClient, private router: Router) {}
 
     getPosts() {
         this.http.get<{message:string, posts: any }>('http://localhost:3000/api/posts')
@@ -32,6 +31,10 @@ export class PostsService {
             })
     }
 
+    getPost(id: string) {
+        return this.http.get<{_id:string, title:string, content:string}>('http://localhost:3000/api/posts/' + id)
+    }
+
     getPostUpdateListener() {
         return this.postsUpdated.asObservable();
     }
@@ -43,7 +46,8 @@ export class PostsService {
                 const id = responsedata.postId;
                 post.id = id;
                 this.posts.push(post);
-                this.postsUpdated.next([...this.posts])
+                this.postsUpdated.next([...this.posts]);
+                this.router.navigate(['/']);
             });
         
     }
@@ -54,6 +58,19 @@ export class PostsService {
                 const updatedPosts = this.posts.filter(post => post.id !== postId);
                 this.posts = updatedPosts;
                 this.postsUpdated.next([...this.posts]);
+            });
+    }
+
+    updatePost(id:string, title:string, content:string) {
+        const post = {id: id, title: title, content: content};
+        this.http.put('http://localhost:3000/api/posts/' + id, post)
+            .subscribe(response => {
+                const updatedPosts = [...this.posts];
+                const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+                updatedPosts[oldPostIndex] = post;
+                this.posts = updatedPosts;
+                this.postsUpdated.next([...this.posts]);
+                this.router.navigate(['/']);
             });
     }
 }
